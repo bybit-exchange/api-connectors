75b729cf628b73964058ed1828a7e70a52ee1188
simon.zhang <simon.zhang@bybit.com>
2019-10-17 14:54:53 +0800

    amend

--- official-http/cpp/main.cpp
+++ official-http/cpp/main.cpp
@@ -8,19 +8,19 @@ using std::endl;
 
 
 int main(){
-    BybitGateway client("xf0e6kounLDRA6hX3OhDLFSKX3rMXY8yrOwV", "gpuHcPpgKucOeaDJ1L");
-    // cout<<client.OnRspChgPositionMargin("BTCUSD", 2)<<endl;
+    BybitGateway client("", "");
+    cout<<client.OnRspChgPositionMargin("BTCUSD", 2)<<endl;
     cout<<client.OnRspQryPosition()<<endl;
-    // cout<<client.OnRspQryLeverage()<<endl;
-    // cout<<client.OnRspChgLeverage("BTCUSD", 20)<<endl;
-    // Order order;
-    // order.order_type = "Limit";
-    // order.price = 10400.5;
-    // order.qty = 1;
-    // order.side = "Buy";
-    // order.symbol = "BTCUSD";
-    // order.time_in_force = "GoodTillCancel";
-    // cout<<client.OnOrder(order)<<endl;
+    cout<<client.OnRspQryLeverage()<<endl;
+    cout<<client.OnRspChgLeverage("BTCUSD", 20)<<endl;
+    Order order;
+    order.order_type = "Limit";
+    order.price = 10400.5;
+    order.qty = 1;
+    order.side = "Buy";
+    order.symbol = "BTCUSD";
+    order.time_in_force = "GoodTillCancel";
+    cout<<client.OnOrder(order)<<endl;
 
     return 0;
 }
