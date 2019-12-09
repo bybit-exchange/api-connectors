a689739ca50bf91bb896f187b1656d716debb7a7
dpuyosa <dpuyosa@users.noreply.github.com>
2019-12-09 19:59:19 +0100

    Use callbacks instead of pooling
    Pooling inst good when you want realtime data. Now you can register a callback for a topic. When the topic has a callback, using `get_data` will return nothing for that topic.

--- official-ws/python/BybitWebsocket.py
+++ official-ws/python/BybitWebsocket.py
@@ -105,9 +105,12 @@ class BybitWebsocket:
                 self.data["pong"].append("PING success")
 
         if 'topic' in message:
-            self.data[message["topic"]].append(message["data"])
-            if len(self.data[message["topic"]]) > BybitWebsocket.MAX_DATA_CAPACITY:
-                self.data[message["topic"]] = self.data[message["topic"]][BybitWebsocket.MAX_DATA_CAPACITY//2:]
+             if message['topic'] in self.callbacks:
+                self.callbacks[message['topic']](message["data"])
+            else:
+                self.data[message["topic"]].append(message["data"])
+                if len(self.data[message["topic"]]) > BybitWebsocket.MAX_DATA_CAPACITY:
+                    self.data[message["topic"]] = self.data[message["topic"]][BybitWebsocket.MAX_DATA_CAPACITY//2:]
 
     def __on_error(self, error):
         '''Called on fatal websocket errors. We exit on these.'''
@@ -206,3 +209,9 @@ class BybitWebsocket:
             # while len(self.data[topic]) == 0 :
             #     sleep(0.1)
             return self.data[topic].pop()
+        
+    def add_callback(self, topic, callback):
+        self.callbacks[topic] = callback
+
+    def remove_callback(self, topic):
+        self.callbacks.pop(topic, None)
